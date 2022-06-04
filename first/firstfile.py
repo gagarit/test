b88d38a79f866865a2998ba18f77666e9b8615c6
VM <markidonov.vm@cyberform.systems>
2022-06-04 12:55:13 +0700

    new functions

--- first/firstfile.py
+++ first/firstfile.py
@@ -1,2 +1,11 @@
 def me(a,b):
-    return a+b
+    print(a+b)
+
+x = 56
+y = x + (2*x)
+
+def answer(y):
+    print(y)
+
+me(1,2)
+answer(4)
---
+++ main.py
@@ -0,0 +1,12 @@
+def me(a,b):
+    print(a+b)
+
+x = 56
+y = x + (2*x)
+print(y)
+
+def answer(y):
+    print(y)
+
+me(1,2)
+answer(4)
